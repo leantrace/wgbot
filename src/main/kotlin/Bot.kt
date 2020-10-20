368016627ebcf778d81e87192398c52895f34dac
Alexander Schamne <alexander.schamne@gmail.com>
2020-10-20 17:40:16 +0200

    test deploy

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -21,46 +21,48 @@ class Bot : TelegramLongPollingBot() {
     private val taste = listOf("taste", "geschmack", "durst", "drink", "hungry", "hunger", "eat", "essen")
     private val members = listOf("sabi")
 
-    override fun getBotToken() = System.getenv("BOT_TOKEN")
-    override fun getBotUsername(): String {
-        println(System.getenv("BOT_USER"))
-        return System.getenv("BOT_USER")
+    init {
+        println("Username: " + System.getenv("BOT_USER"))
+        println("Token: " + System.getenv("BOT_TOKEN"))
     }
 
-        override fun onUpdateReceived(update: Update) {
-            if (update.message?.text != null) {
-                val text = update.message.text.toLowerCase()
-                println(text)
-                val hated = hates.find { it in text }
-                val loved = loves.find { it in text }?.let { it.find(text)!!.value }
-                val chatId = update.message.chatId
+    override fun getBotToken() = System.getenv("BOT_TOKEN")
+    override fun getBotUsername() = System.getenv("BOT_USER")
 
-                fun send(text: String) = execute(SendMessage(chatId, text))
+    override fun onUpdateReceived(update: Update) {
+        if (update.message?.text != null) {
+            val text = update.message.text.toLowerCase()
+            println(text)
+            val hated = hates.find { it in text }
+            val loved = loves.find { it in text }?.let { it.find(text)!!.value }
+            val chatId = update.message.chatId
 
-                fun sendImage(name: String, caption: String) = execute(SendPhoto().apply {
-                    setChatId(chatId)
-                    setCaption(caption)
-                    setPhoto(name, Thread.currentThread().contextClassLoader.getResourceAsStream(name))
-                })
+            fun send(text: String) = execute(SendMessage(chatId, text))
 
-                when {
-                    text.startsWith("/help") -> send("Say something about me, beer or ch.effingerbot.rocks.getJokes. I'll try to answer...")
-                    react.any { it in text } -> send(quotes.choose())
-                    beer.any { it in text } -> send("Can I have a ${country.choose()} beer, please?")
-                    //members.any { it in text } -> send("${text} setz einen Kaffee auf, trink ihn und mach Vegane Burger!")
-                    members.any { it in text } -> send("${text} musst du nicht in Beans & Nuts?")
-                    hated != null -> send("I hate ${hated}!")
-                    loved != null -> send("I love ${loved}!")
-                    taste.any { it in text } -> send("Try me! I have an excellent taste!")
-                    listOf("joke", "witz").any { it in text } -> jokes.choose().let {
-                        if (it.image == null) send(it.text)
-                        else sendImage(it.image, it.text)
-                    }
-                    rnd.nextDouble() < .1 -> send(quotes.choose())
+            fun sendImage(name: String, caption: String) = execute(SendPhoto().apply {
+                setChatId(chatId)
+                setCaption(caption)
+                setPhoto(name, Thread.currentThread().contextClassLoader.getResourceAsStream(name))
+            })
+
+            when {
+                text.startsWith("/help") -> send("Say something about me, beer or ch.effingerbot.rocks.getJokes. I'll try to answer...")
+                react.any { it in text } -> send(quotes.choose())
+                beer.any { it in text } -> send("Can I have a ${country.choose()} beer, please?")
+                //members.any { it in text } -> send("${text} setz einen Kaffee auf, trink ihn und mach Vegane Burger!")
+                members.any { it in text } -> send("${text} musst du nicht in Beans & Nuts?")
+                hated != null -> send("I hate ${hated}!")
+                loved != null -> send("I love ${loved}!")
+                taste.any { it in text } -> send("Try me! I have an excellent taste!")
+                listOf("joke", "witz").any { it in text } -> jokes.choose().let {
+                    if (it.image == null) send(it.text)
+                    else sendImage(it.image, it.text)
                 }
+                rnd.nextDouble() < .1 -> send(quotes.choose())
             }
         }
-
-        private fun <T> List<T>.choose() = this[rnd.nextInt(size)]
     }
+
+    private fun <T> List<T>.choose() = this[rnd.nextInt(size)]
+
 }
